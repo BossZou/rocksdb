0607506b69fad19521427579ce626e58bb433b4d
yinghao.zou <yinghao.zou@zilliz.com>
2020-12-28 11:10:31 +0800

    latenct
    Signed-off-by: yinghao.zou <yinghao.zou@zilliz.com>

--- labs/lab2_test.cc
+++ labs/lab2_test.cc
@@ -2,6 +2,7 @@
 // Created by yhz on 2020-12-27.
 //
 
+#include <algorithm>
 #include <chrono>
 #include <experimental/filesystem>
 #include <iomanip>
@@ -73,7 +74,7 @@ int main() {
       std::cout << "< " << i << "/" << total_count << " > ... " << std::endl;
     }
 
-    if (i % 10000 == 0) {
+    if (i % 1000 == 0) {
       keys.emplace_back(key);
     }
 
@@ -101,19 +102,47 @@ int main() {
     auto duration =
         std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
-    std::cout << std::setw(10) << "result" << std::setw(20) << "total time(ms)"
+    std::cout << std::setw(10) << "result" << std::setw(20) << "total count" << std::setw(20) << "total time"
               << std::setw(40) << "value"
               << "\n"
-              << std::setw(10) << "QPS" << std::setw(20) << duration.count() * 1000
+              << std::setw(10) << "QPS" << std::setw(20) << keys.size()
+              << std::setw(20) << std::to_string(duration.count() / 1000.0f) + " ms"
               << std::setw(40)
               << std::to_string(keys.size() / float(duration.count()) * 1000 * 1000) +
                      " entities / s"
               << "\n"
-              << std::setw(10) << "Latency" << std::setw(20) << duration.count() * 1000
+              << std::setw(10) << "Latency" << std::setw(20) << keys.size()
+              << std::setw(20) << std::to_string(duration.count() / 1000.0f) + " ms"
               << std::setw(40)
               << std::to_string(duration.count() / (float(keys.size()))) +
                      " us / entities"
               << std::endl;
+  }
+
+  for (size_t m = 0; m < 5; m++) {
+    std::vector<size_t> latencies;
+    ReadOptions r_options;
+    std::string v;
+    for (auto& kkey : keys) {
+      auto l_begin = std::chrono::high_resolution_clock::now();
+      db->Get(r_options, kkey, &v);
+      auto l_end = std::chrono::high_resolution_clock::now();
+      auto l_duration = std::chrono::duration_cast<std::chrono::microseconds>(
+          l_end - l_begin);
+      latencies.emplace_back(l_duration.count());
+    }
+
+    auto max = std::max_element(std::begin(latencies), std::end(latencies));
+    auto min = std::min_element(std::begin(latencies), std::end(latencies));
+    size_t t_v = 0;
+    for (auto& l : latencies) {
+      t_v += l;
+    }
+
+    std::cout << "\n\nLatency:\n"
+              << "=== Round " << m << " ===\n\t"
+              << "Max: " << *max << ", Min: " << *min
+              << ", Avg: " << t_v / float(latencies.size()) << std::endl;
   }
 
   return 0;
